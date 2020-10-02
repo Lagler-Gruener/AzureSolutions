eee46f8c75eb5648746e644284ab76e6e9bf5ff2
Johannes Lagler-Grüner <johannes.lagler-gruener@acp.at>
2020-10-02 13:09:58 +0200

    V1

---
+++ Mappingtool/PSScripts/AzureAutomation/UpdateMappingTool.ps1
@@ -0,0 +1,20 @@
+<#
+    .SYNOPSIS
+       Initial script to check rg memberships.
+        
+    .DESCRIPTION
+        Script was executed by Azure Logic App.       
+            
+            
+
+    .EXAMPLE
+        -    
+
+    .NOTES  
+        Required modules: 
+             
+
+        Required permissions:          
+            
+                                   
+#>
