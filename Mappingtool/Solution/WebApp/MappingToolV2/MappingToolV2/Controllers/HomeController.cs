6b4f375eb0db4a2ef32bf610971dc7c7b045b6a5
Hannes Lagler-Gruener <hannes@gruener.eu.org>
2021-06-21 16:43:38 +0200

    Initial create WebApp tool V2

---
+++ Mappingtool/Solution/WebApp/MappingToolV2/MappingToolV2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MappingToolV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+
+        public ActionResult Architecture()
+        {
+            return View();
+        }
+
+        public ActionResult Workflows()
+        {
+            return View();
+        }
+
+        public ActionResult State()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mappingtool/Solution/WebApp/MappingToolV2/MappingToolV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MappingToolV2.MvcApplication" Language="C#" %>
